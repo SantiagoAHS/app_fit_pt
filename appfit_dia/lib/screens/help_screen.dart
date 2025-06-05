61a2ffc69356318965e3599ed900c0e9cc01eab0
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2025-06-04 22:18:12 -0600

    Create help-screen mobile

--- appfit_dia/lib/screens/help_screen.dart
+++ appfit_dia/lib/screens/help_screen.dart
@@ -32,14 +32,64 @@ class HelpScreen extends StatelessWidget {
   }
 
   Widget _buildMobileLayout() {
-    return const Center(
-      child: Text(
-        'Bienvenido a la pantalla de ayuda',
-        style: TextStyle(fontSize: 16),
-        textAlign: TextAlign.center,
-      ),
+    final faqs = [
+    {
+      'question': '¿Cómo empiezo con mi primera rutina de ejercicio?',
+      'answer': 'Después de registrarte, dirígete a la sección “Rutinas” y selecciona la de tu agrado. Recomendamos comenzar con la rutina de principiantes si no has entrenado recientemente.',
+    },
+    {
+      'question':'¿Puedo entrenar sin equipo?',
+      'answer':'Sí, muchas de nuestras rutinas están diseñadas para realizarse en casa sin ningún equipo.',
+    },
+    {
+      'question':'¿Cuántos días a la semana debo entrenar?',
+      'answer':'Depende de tu nivel y tus objetivos. Para principiantes, recomendamos 3-4 días a la semana. Los intermedios o avanzados pueden entrenar 5-6 días.',
+    },
+    {
+      'question':'¿Qué pasa si me salto un día?',
+      'answer':'No te preocupes, la consistencia es más importante que la perfección. Puedes retomar desde el día siguiente sin problema.',
+    },
+    {
+      'question':'¿Cómo puedo llevar un seguimiento de mi progreso?',
+      'answer':'Puedes acceder a la sección de "Estadisticas" para ver un seguimiento de tu progeso.',
+    },
+    {
+      'question': '¿Por qué no puedo acceder a mis estadisticas?',
+      'answer': 'Para acceder a tus estadisticas personales debes primero ingresar tus datos en la sección de "Perfil"',
+    },
+    {
+      'question':'¿Alguna otra duda?',
+      'answer':'Contactanos al: "241-176-4468"',
+    }
+    
+
+    ];
+
+  return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: faqs.length,
+      itemBuilder: (context, index) {
+        final faq = faqs[index];
+        return Card(
+          elevation: 2,
+          margin: const EdgeInsets.only(bottom: 12),
+          child: ExpansionTile(
+            title: Text(
+              faq['question']!,
+              style: const TextStyle(fontWeight: FontWeight.w600),
+            ),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(faq['answer']!),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
+  
 
   Widget _buildLargeScreenLayout() {
     return const Center(
